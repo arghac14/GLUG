d9495f5c90814f3fe52522318095801d17a977be
sohelraja <rohan.nsec@gmail.com>
2019-03-09 11:57:15 +0530

    Navbar Setup

---
+++ asserts/js/main.js
@@ -0,0 +1,11 @@
+var toggle = document.getElementById("myToggle");
+toggle.addEventListener("click",myToggler);
+function myToggler(e){
+    var myEvent = e.target;
+    if(myEvent.className === 'btn-open'){
+        myEvent.className = 'btn-close';
+    }
+    else if(myEvent.className === 'btn-close'){ 
+        myEvent.className = 'btn-open';
+    }
+}
